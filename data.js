60fe25164a8e40c5f370131628b2e54287e32fbd
Jasmin Dedic <jdedic1999@gmail.com>
2022-10-13 12:35:25 +0200

    Finished overall design and some logic

---
+++ data.js
@@ -0,0 +1,43 @@
+/* JSON Array */
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
+        "answer_1": "Test Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie bindest man eine Website in eine Website ein?",
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2": "&lt;iframe&gt;",
+        "answer_3": "&lt;frame&gt;",
+        "answer_4": "&lt;frameset&gt;",
+        "right_answer": 2
+    },
+    {
+        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer_1": "readonly",
+        "answer_2": "max",
+        "answer_3": "from",
+        "answer_4": "spellcheck",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
+        "answer_1": "a[title]{...}",
+        "answer_2": "a > title {...}",
+        "answer_3": "a.title {...}>",
+        "answer_4": "a=title {...}",
+        "right_answer": 1
+    },
+];
--- script.js
+++ script.js
@@ -1,7 +1,71 @@
 
-
+let currentQuestion = 0;
 
 // Init function to run all generating functions
 function init() {
-    generateHeader()
+    generateHeader();
+    questionsAmount();
+    showQuestion();
+}
+
+// Update questions amount
+function questionsAmount() {
+    document.getElementById("all-questions").innerHTML = questions.length;
+}
+
+function showQuestion() {
+
+    if (currentQuestion >= question.length) {
+        // Show end screen
+    } else {
+
+
+        let question = questions[currentQuestion];
+
+        document.getElementById("question-number").innerHTML = currentQuestion + 1;
+        document.getElementById("questionText").innerHTML = question["question"];
+        document.getElementById("answer_1").innerHTML = question["answer_1"];
+        document.getElementById("answer_2").innerHTML = question["answer_2"];
+        document.getElementById("answer_3").innerHTML = question["answer_3"];
+        document.getElementById("answer_4").innerHTML = question["answer_4"];
+    }
+
+}
+// Function to check if answer is right
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question["right_answer"]}`;
+
+    if (selectedQuestionNumber == question["right_answer"]) {
+        document.getElementById(selection).parentNode.classList.add("bg-success");
+    } else {
+        document.getElementById(selection).parentNode.classList.add("bg-danger");
+        document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    }
+
+    document.getElementById("next-button").disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++; //zb. von 0 auf 1
+
+    document.getElementById("next-button").disabled = true;
+    resetAnswerButtons();
+    showQuestion();
+
+
+}
+
+function resetAnswerButtons() {
+    document.getElementById("answer_1").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_2").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_3").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+    document.getElementById("answer_4").parentNode.classList.remove("bg-danger");
+    document.getElementById("answer_4").parentNode.classList.remove("bg-success");
+
+
 }
